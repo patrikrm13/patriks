f0a5b79b42bacc4bcf63367be3aa353b2c7991ba
patrikrm13 <mahdi028a@gmail.com>
2016-02-17 17:39:49 +0330

    Create nerkh.lua

---
+++ plugins/nerkh.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return " قیمت هر گروه 5 هزار شارژ رایتل "
+  end
+return {
+  description = "poker!", 
+  usage = "fuck : ",
+  patterns = {
+    "^nerkh",
+  },
+  run = run
+}
+end
